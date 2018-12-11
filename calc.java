ca72313710201c941728e5b0a625277cd202f429
Joshua Birdine <joshbird5@gmail.com>
2018-12-11 15:14:18 -0800

    fix calc.java

--- calc.java
+++ calc.java
@@ -1,1 +1,23 @@
-# calc
+
+public class calc {
+	public int add(int a, int b) {
+		return a + b;
+		}
+	public int subtract(int a, int b) {
+		return a - b;
+		}
+	public int multiply(int a, int b) {
+		return a * b;
+		}
+	public int divide(int a, int b) {
+		return a / b;
+		}
+
+	public static void main(String[] args) {
+		calc myCalculator = new calc();
+		System.out.println(myCalculator.add(4,2));
+		System.out.println(myCalculator.subtract(7,5));
+		System.out.println(myCalculator.multiply(8,4));
+		System.out.println(myCalculator.divide(35,5));
+	}
+}
